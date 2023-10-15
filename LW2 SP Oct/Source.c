6af344f6052c6b90a7cc0084bd54d4ebb3a8d9e0
Yana Malashkina <jana.malashkina@gmail.com>
2023-10-15 20:25:35 +0200

    Added request for a file location

--- LW2 SP Oct/Source.c
+++ LW2 SP Oct/Source.c
@@ -1,78 +1,92 @@
 #include <stdio.h>
-#include <locale.h>
+#include <string.h>
 
-int isAccepted(char* w0, char* w1) {
-	int currentState = 0;
+#define MAX_STATES 100
+#define MAX_ALPHABET 256  // Змінено розмір алфавіту для кирилиці
+#define MAX_LENGTH 100
 
-	wprintf(L"Початок перевірки:\n");
+struct Transition {
+	int fromState;
+	char symbol;
+	int toState;
+};
 
-	for (int i = 0; w1[i] != '\0'; ++i) {
-		switch (currentState) {
-		case 0:
-			if (w1[i] == 'a') {
-				currentState = 1;
-				wprintf(L"Перехід зі стану 0 в стан 1 (a)\n");
+int isAccepted(char* w0, struct Transition transitions[MAX_STATES][MAX_ALPHABET], int initialState, int finalState) {
+	int currentState = initialState;
+
+	// Пройти через кожен символ слова і виконати перехід в автоматі
+	for (int i = 0; i < strlen(w0); ++i) {
+		char symbol = w0[i];
+
+		// Знайти відповідний перехід
+		int found = 0;
+		for (int j = 0; j < MAX_ALPHABET; ++j) {
+			if (transitions[currentState][j].symbol == symbol) {
+				currentState = transitions[currentState][j].toState;
+				found = 1;
+				break;
 			}
-			else {
-				wprintf(L"Немає переходу зі стану 0 за символом %c\n", w1[i]);
-				return 0;
-			}
-			break;
-		case 1:
-			if (w1[i] == 'a') {
-				currentState = 2;
-				wprintf(L"Перехід зі стану 1 в стан 2 (a)\n");
-			}
-			else if (w1[i] == 'b') {
-				currentState = 3;
-				wprintf(L"Перехід зі стану 1 в стан 3 (b)\n");
-			}
-			else {
-				wprintf(L"Немає переходу зі стану 1 за символом %c\n", w1[i]);
-				return 0;
-			}
-			break;
-		case 2:
-			if (w1[i] == 'b') {
-				currentState = 1;
-				wprintf(L"Перехід зі стану 2 в стан 1 (b)\n");
-			}
-			else {
-				wprintf(L"Немає переходу зі стану 2 за символом %c\n", w1[i]);
-				return 0;
-			}
-			break;
-		case 3:
-			if (w1[i] == 'a') {
-				currentState = 1;
-				wprintf(L"Перехід зі стану 3 в стан 1 (a)\n");
-			}
-			else {
-				wprintf(L"Немає переходу зі стану 3 за символом %c\n", w1[i]);
-				return 0;
-			}
-			break;
+		}
+
+		// Якщо не знайдено відповідного переходу, слово не допускається
+		if (!found) {
+			return 0;
 		}
 	}
 
-	if (currentState == 2 && w0[0] == '2' && w0[1] == '4' && w0[2] == '\0') {
-		wprintf(L"Слово w1w0 допускається\n");
-		return 1;
-	}
-
-	wprintf(L"Слово w1w0 не допускається\n");
-
-	return 0;
+	// Перевірка, чи завершили ми в фінальному стані
+	return currentState == finalState;
 }
 
 int main() {
-	// Встановлення локалі для підтримки кирилиці
-	setlocale(LC_ALL, "");
+	int numStates, numTransitions;
+	int initialState, finalState;
+	char filename[50]; // Назва файлу
 
-	char w0[] = "24";
-	char w1[] = "ababaa";
+	// Запит користувача про ім'я файлу
+	printf("Enter the filename with the automaton description: ");
+	scanf_s("%s", filename, sizeof(filename));
 
-	isAccepted(w0, w1);
+	FILE* file;
+	if (fopen_s(&file, filename, "r") != 0) {
+		perror("Error opening file");
+		return 1;
+	}
+
+	// Зчитування кількості станів і переходів
+	fscanf_s(file, "%d %d", &numStates, &numTransitions);
+
+	// Зчитування початкового і кінцевого станів
+	fscanf_s(file, "%d %d", &initialState, &finalState);
+
+	// Ініціалізація масиву переходів
+	struct Transition transitions[MAX_STATES][MAX_ALPHABET] = { 0 };
+
+	// Зчитування переходів
+	for (int i = 0; i < numTransitions; ++i) {
+		int fromState, toState;
+		char symbol;
+		fscanf_s(file, "%d %c %d", &fromState, &symbol, sizeof(symbol), &toState);
+		transitions[fromState][symbol].fromState = fromState;
+		transitions[fromState][symbol].symbol = symbol;
+		transitions[fromState][symbol].toState = toState;
+	}
+
+	fclose(file); // Закриття файлу
+
+	char w0[MAX_LENGTH];
+
+	// Введення w0
+	printf("Enter the word w0: ");
+	scanf_s("%s", w0, sizeof(w0));
+
+	// Перевірка та виведення результату
+	if (isAccepted(w0, transitions, initialState, finalState)) {
+		printf("The word w = %s is accepted by the finite automaton.\n", w0);
+	}
+	else {
+		printf("The word w = %s is not accepted by the finite automaton.\n", w0);
+	}
 
 	return 0;
 }
